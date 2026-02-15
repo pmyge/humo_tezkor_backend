37bdf92df1f11ef7d6bf46a79943e5fb4c489023
Dilshodbek01 <dilshodbekjumanov11@gmail.com>
2026-02-16 01:11:04 +0500

    Fix: Ultimate cleanup of urls.py imports and health check

--- config/urls.py
+++ config/urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
-from django.views.static import serve
 from django.urls import path, include, re_path
 from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 from django.http import HttpResponse
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 def health_check(request):
     return HttpResponse("OK")
@@ -39,8 +24,7 @@ urlpatterns = [
     path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
-from django.views.static import serve
-
+# Serve media files in production manually to ensure they are accessible
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
