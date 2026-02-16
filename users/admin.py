28d621d3cbb1035dd653dbf203b19e2128756617
Dilshodbek01 <dilshodbekjumanov11@gmail.com>
2026-02-16 16:46:43 +0500

    Admin: remove last_name and fix fallback ID logic

--- users/admin.py
+++ users/admin.py
@@ -15,9 +15,9 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ('telegram_user_id', 'first_name', 'last_name', 'phone_number', 'created_at')
+    list_display = ('telegram_user_id', 'first_name', 'phone_number', 'created_at')
     list_filter = ('created_at',)
-    search_fields = ('telegram_user_id', 'first_name', 'last_name', 'phone_number', 'username')
+    search_fields = ('telegram_user_id', 'first_name', 'phone_number', 'username')
     readonly_fields = ('created_at', 'updated_at')
     
     def get_queryset(self, request):
@@ -26,7 +26,7 @@ class CustomerAdmin(admin.ModelAdmin):
     
     fieldsets = (
         ('Telegram Info', {
-            'fields': ('telegram_user_id', 'username', 'first_name', 'last_name')
+            'fields': ('telegram_user_id', 'username', 'first_name')
         }),
         ('Contact', {
             'fields': ('phone_number',)
