b9b47514622c3144c6a98e70ff3b0832994f9395
Mirshoxid <mirshoxid@example.com>
2026-03-01 21:03:02 +0500

    Downgrade Django to 5.1.7 for Jazzmin compatibility and disable DEBUG

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--gc%zx-#!-dm7khjntk-n(rvd$$(&lj3fsyr&upgc)%artzke^')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     "localhost",
