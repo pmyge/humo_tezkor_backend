4899afceee43384a427ed66a751e67743e3f1ec9
Mirshoxid <mirshoxid@example.com>
2026-03-01 21:00:17 +0500

    Fix settings.py and enable DEBUG mode

--- config/settings.py
+++ config/settings.py
@@ -25,9 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure--gc%zx-#!-dm7khjntk-n(rvd$$(&lj3fsyr&upgc)%artzke^')
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
